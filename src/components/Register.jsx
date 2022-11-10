95ec1b64be497beaee598e1004d90cb1a83c4729
Jackson Boehman <jpboehman@gmail.com>
2022-11-10 16:47:43 -0500

    styling for register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -111,80 +111,78 @@ function Register() {
 	};
 
 	return (
-		<div className="col-md-12">
-			<div className="card card-container">
-				<img
-					src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
-					alt="profile-img"
-					className="profile-img-card"
-					style={{
-						height: '100px',
-						width: '100px',
-						objectFit: 'cover',
-						marginLeft: '50%'
-					}}
-				/>
+		<div>
+			<img
+				src="//ssl.gstatic.com/accounts/ui/avatar_2x.png"
+				alt="profile-img"
+				className="profile-img-card"
+				style={{
+					height: '100px',
+					width: '100px',
+					objectFit: 'cover',
+					marginLeft: '50%'
+				}}
+			/>
 
-				<Card title={`Register`}>
-					<Form
-						onSubmit={handleRegister}
-						ref={form}
-						layout="vertical"
-						size="default"
-						className={css`margin-bottom: 15px;`}
-					>
-						<div>
-							<div className="form-group">
-								<label htmlFor="username">Username</label>
-								<Input
-									type="text"
-									className="form-control"
-									name="username"
-									value={username}
-									onChange={onChangeUsername}
-									validations={[ required, vusername ]}
-								/>
-							</div>
+			<Card title={`Register`}>
+				<Form
+					onSubmit={handleRegister}
+					ref={form}
+					layout="vertical"
+					size="default"
+					className={css`margin-bottom: 15px;`}
+				>
+					<div>
+						<div className="form-group">
+							<label htmlFor="username">Username</label>
+							<Input
+								type="text"
+								className="form-control"
+								name="username"
+								value={username}
+								onChange={onChangeUsername}
+								validations={[ required, vusername ]}
+							/>
+						</div>
 
-							<div className="form-group">
-								<label htmlFor="email">Email</label>
-								<Input
-									type="text"
-									className="form-control"
-									name="email"
-									value={email}
-									onChange={onChangeEmail}
-									validations={[ required, validEmail ]}
-								/>
-							</div>
+						<div className="form-group">
+							<label htmlFor="email">Email</label>
+							<Input
+								type="text"
+								className="form-control"
+								name="email"
+								value={email}
+								onChange={onChangeEmail}
+								validations={[ required, validEmail ]}
+							/>
+						</div>
 
-							<div className="form-group">
-								<label htmlFor="password">Password</label>
-								<Input
-									type="password"
-									className="form-control"
-									name="password"
-									value={password}
-									onChange={onChangePassword}
-									validations={[ required, vpassword ]}
-								/>
-							</div>
+						<div className="form-group">
+							<label htmlFor="password">Password</label>
+							<Input
+								type="password"
+								className="form-control"
+								name="password"
+								value={password}
+								onChange={onChangePassword}
+								validations={[ required, vpassword ]}
+							/>
+						</div>
 
-							<div className="form-group">
-								<button className="btn btn-primary btn-block">Sign Up</button>
+						<div className="form-group">
+							<button className="btn btn-primary btn-block">Sign Up</button>
+						</div>
+					</div>
+					{message && (
+						<div className="form-group">
+							<div className={successful ? 'alert alert-success' : 'alert alert-danger'} role="alert">
+								{message}
 							</div>
 						</div>
-						{message && (
-							<div className="form-group">
-								<div className={successful ? 'alert alert-success' : 'alert alert-danger'} role="alert">
-									{message}
-								</div>
-							</div>
-						)}
-						<CheckButton style={{ display: 'none' }} ref={checkBtn} />
-					</Form>
-				</Card>
-			</div>
+					)}
+					<CheckButton style={{ display: 'none' }} ref={checkBtn} />
+				</Form>
+			</Card>
 		</div>
 	);
 }
